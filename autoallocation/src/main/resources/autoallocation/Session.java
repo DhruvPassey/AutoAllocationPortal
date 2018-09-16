faab101d3d733ad42b585c15b10efd6a6cdc812e
Mehak Mahajan <mehakmahajan4038@gmail.com>
2018-09-16 18:47:31 +0530

    DAO resources

---
+++ autoallocation/src/main/resources/autoallocation/Session.java
@@ -0,0 +1,28 @@
+package autoallocation;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class Session {
+
+	private SqlSessionFactory getSqlSession() {
+		String resource = "mybatis-config.xml";
+		InputStream inputStream = null;
+		try {
+			inputStream = Resources.getResourceAsStream(resource);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+		return sqlSessionFactory;
+	}
+	
+	public SqlSession openSqlSession() {
+		return getSqlSession().openSession();
+	}
+}
